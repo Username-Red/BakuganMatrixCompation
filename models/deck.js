f5932a79b1e3e89283b6a5efbab0730b03cfa967
Username-Red <ashkestrel@gmail.com>
2025-08-04 19:16:57 +0200

    fixing deck validation

--- models/deck.js
+++ models/deck.js
@@ -1,30 +1,30 @@
 const mongoose = require('mongoose');
 
-const itemSchema = new mongoose.Schema({
-    bakugan: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Bakugan',
-        validate: {
-            validator: arr => arr.length === 3,
-            message: 'Deck must contain exactly 3 Bakugan'
-        }
-    }],
-    gates: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Gate',
-        validate: {
-            validator: arr => arr.length === 3,
-            message: 'Deck must contain exactly 3 Gate cards'
-        }
-    }],
-    cores: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Core',
-        validate: {
-            validator: arr => arr.length === 3,
-            message: 'Deck must contain exactly 3 BakuCores'
-        }
-    }]
+const deckSchema = new mongoose.Schema({
+  bakugan: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'Bakugan',
+    validate: {
+      validator: arr => arr.length === 3,
+      message: 'Deck must contain exactly 3 Bakugan'
+    }
+  },
+  gates: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'Gate',
+    validate: {
+      validator: arr => arr.length === 3,
+      message: 'Deck must contain exactly 3 Gate cards'
+    }
+  },
+  cores: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'Core',
+    validate: {
+      validator: arr => arr.length === 3,
+      message: 'Deck must contain exactly 3 BakuCores'
+    }
+  }
 });
 
-module.exports = mongoose.model('Deck', itemSchema);
+module.exports = mongoose.model('Deck', deckSchema);
